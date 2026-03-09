b0febd4d524a4cf5d1aa7aca09c30300a568dbbe
ssjblue46 <raj.tamboskar@somaiya.edu>
2026-03-09 09:55:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
+
 
 app.use(cors());
 app.use(express.json());
@@ -341,8 +343,11 @@ app.delete("/api/pdfs/:filename", (req, res) => {
   });
 });
 
+app.use(express.static(path.join(__dirname, "build")));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
