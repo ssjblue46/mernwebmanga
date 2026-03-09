3b206d4627de37b667b6f0335683f441ba3db861
ssjblue46 <raj.tamboskar@somaiya.edu>
2026-03-09 12:12:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -343,11 +343,6 @@ app.delete("/api/pdfs/:filename", (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
